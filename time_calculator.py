3e50a8e8d02dfaf40eabbb3601d929cd90fe9ece
Chris Zichko <75284822+ZichKoding@users.noreply.github.com>
2021-02-03 13:36:30 -0500

    Update time_calculator.py
    Woooo! Finally complete! Add your start time for in for the first argument and the length of time (hr:min) to the second argument and it'll tell you what time it'll be in 12hr format. There is a third, optional argument for if you, also, want to calculate the weekday, but you have to enter the day of the week you'd like to calculate from ( add_time("1:45 PM", "1879:25", "Monday").

--- time_calculator.py
+++ time_calculator.py
@@ -1,6 +1,11 @@
-def add_time(start, duration, **kwarg): 
-
-#### This is not a finished function yet. missing the optional argument for what day of the week. ####
+def add_time(start, duration, weekday=None):
+  ### If you want to find what day of the week you want you have to pass in what day to start with. ###
+    day_of_week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    if weekday is not None:
+        weekday = weekday.lower().capitalize()
+        for day_o_week in day_of_week:
+            if weekday == day_o_week:
+                weekday = day_of_week.index(weekday)
 
     # breaking down the start time for ease of use later.
     for t in start:
@@ -20,7 +25,7 @@ def add_time(start, duration, **kwarg):
     # Break down the duration
     for hours in duration:
         if hours == ':':
-            add_hours = duration.split(':') 
+            add_hours = duration.split(':')
             amount_hours = add_hours[0]
             amount_minutes = add_hours[1]
             amount_days = 0
@@ -35,54 +40,73 @@ def add_time(start, duration, **kwarg):
     total_hours = (int(start_hour) + int(amount_hours))
     total_minutes = (int(start_minutes) + int(amount_minutes))
     days = amount_days
+
     # Calculating and formatting minutes.
     if int(total_minutes) >= 60:
         total_hours = (int(total_hours) + (int(total_minutes) // 60))
         sixty_min_format = (int(total_minutes) % 60)
         if int(sixty_min_format) < 10:
-          sixty_min_format = str('0' + str(sixty_min_format))
+            sixty_min_format = str('0' + str(sixty_min_format))
     else:
-      sixty_min_format = total_minutes
-      if int(sixty_min_format) < 10:
-        sixty_min_format = ('0' + str(sixty_min_format))
+        sixty_min_format = total_minutes
+        if int(sixty_min_format) < 10:
+            sixty_min_format = ('0' + str(sixty_min_format))
 
     # Calculating and formatting hours.
     if int(total_hours) >= 24:
-      days = (int(amount_days) + (int(total_hours) // 24))
-      twentyfour_hr_format = (int(total_hours) % 24)
-      if int(twentyfour_hr_format) > 12:
-        twelve_hr_format = (int(twentyfour_hr_format) - 12)
-      if int(twentyfour_hr_format) <= 12:
-        twelve_hr_format = (twentyfour_hr_format)
+        days = (int(amount_days) + (int(total_hours) // 24))
+        twentyfour_hr_format = (int(total_hours) % 24)
+        if int(twentyfour_hr_format) > 12:
+            twelve_hr_format = (int(twentyfour_hr_format) - 12)
+        if int(twentyfour_hr_format) <= 12:
+            twelve_hr_format = (twentyfour_hr_format)
     if int(total_hours) <= 24:
-      twentyfour_hr_format = total_hours
-      if int(twentyfour_hr_format) > 12:
-        twelve_hr_format = (int(twentyfour_hr_format) - 12)
-      if int(twentyfour_hr_format) <= 12:
-        twelve_hr_format = (twentyfour_hr_format)
+        twentyfour_hr_format = total_hours
+        if int(twentyfour_hr_format) > 12:
+            twelve_hr_format = (int(twentyfour_hr_format) - 12)
+        if int(twentyfour_hr_format) <= 12:
+            twelve_hr_format = (twentyfour_hr_format)
 
     # Determining AM or PM.
     period_of_day = 0
     if int(twentyfour_hr_format) == 24:
-      period_of_day = ' AM'
+        period_of_day = ' AM'
     elif int(twentyfour_hr_format) >= 12:
-      period_of_day = " PM"
+        period_of_day = " PM"
     elif int(twentyfour_hr_format) < 12:
-      period_of_day = " AM"
+        period_of_day = " AM"
 
-
-    # Calculating how many days if any have passed. 
+    # Calculating how many days if any have passed.
     if int(days) == 1:
-      days_passed = ('(next day)')
+        days_passed = ('(next day)')
     if int(days) > 1:
-      days_passed = ('(' + str(days) + ' days later)')
+        days_passed = ('(' + str(days) + ' days later)')
 
-    
+    # Calculating the weekday if its asked.
+    if weekday is not None:
+      if int(days) >= 7:
+        dayofweek = (int(days) % 7)
+        weekday = (int(weekday) + int(dayofweek))
+        if int(weekday) >= 7:
+            weekday = (int(weekday) % 7)
+            weekday = ((int(weekday)))
+        calculated_weekday = day_of_week[weekday]
+      if int(days) < 7:
+        weekday = (int(weekday) + int(days))
+        if int(weekday) >= 7:
+            weekday = (int(weekday) % 7)
+            weekday = ((int(weekday)))
+        calculated_weekday = day_of_week[weekday]
 
-    if int(days) == 0:
-      new_time = (str(twelve_hr_format) + ':' + str(sixty_min_format) + str(period_of_day))
+    # Formatting the solutions.
+      if int(days) == 0:
+        new_time = (str(twelve_hr_format) + ':' + str(sixty_min_format) + str(period_of_day) + ',' + ' ' + str(calculated_weekday))
+      else:
+        new_time = (str(twelve_hr_format) + ':' + str(sixty_min_format) + str(period_of_day) + ',' + ' ' + str(calculated_weekday) + ' ' +str(days_passed))
     else:
-      new_time = (str(twelve_hr_format) + ':' + str(sixty_min_format) + str(period_of_day) + ' ' + str(days_passed))
+      if int(days) == 0:
+        new_time = (str(twelve_hr_format) + ':' + str(sixty_min_format) + str(period_of_day))
+      else:
+        new_time = (str(twelve_hr_format) + ':' + str(sixty_min_format) + str(period_of_day) + ' ' + str(days_passed))
 
-    
     return new_time
